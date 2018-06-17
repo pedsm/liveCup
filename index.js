a8f254875a42a28ee3fd58c679f635a289fdc05c
pedsm <peumendonca@gmail.com>
2018-06-17 23:04:50 +0100

    Better vertical alignment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { Card, Carousel, message } from 'antd'
+import { 
+  Card,
+  Carousel,
+  message,
+  Row,
+  Col
+} from 'antd'
 import Spin from './components/Spin'
 import Group from './components/Group'
 import Game from './components/Game'
@@ -78,8 +84,12 @@ class App extends React.Component {
     const { today } = this.state
     if (today.length > 0) {
       return (
-        <Fragment>
-          {today.map((match, i) => (<Game key={i} match={match} />))}
+        <Fragment >
+          {today.map((match, i) => (
+            <Col key={i} span={8}>
+              <Game match={match} />
+            </Col>
+          ))}
         </Fragment>
       )
     }
@@ -94,22 +104,26 @@ class App extends React.Component {
       now = this.state.now[0]
     } else {
       now = this.state.today.filter(match => match.status != 'completed')[0]
-      if(now == null) {
+      if (now == null) {
         now = this.state.today[this.state.today.length - 1]
       }
     }
     return (
       <div id="main">
-        <div style={{ width: "calc(100% - 300px)", height: "100vh" }}>
+        <div style={{ 
+          width: "calc(100% - 300px)",
+          height: "100vh",
+          display: "grid"
+        }}>
           <div id="header" style={{ textAlign: 'center', padding: 10 }}>
             <h1 style={{ margin: 0 }}>World Cup 2018 Live</h1>
           </div>
           <div style={groupTable}>
             {this.renderGroupTable()}
           </div>
-          <div style={{ display: 'flex', margin: "auto" }}>
+          <Row style={{ marginTop: "auto", marginBottom: "auto" }}>
             {this.renderGames()}
-          </div>
+          </Row>
         </div>
         <div id="current" style={{ width: 300, margin: "auto" }}>
           {(() => {
