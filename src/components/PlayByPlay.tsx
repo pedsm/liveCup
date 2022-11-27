1590bc8422e037fd591355f321d8651c0b9ed688
Pedro Mendonca <peumendonca@gmail.com>
2022-11-27 20:29:37 +0000

    Flip

--- src/components/PlayByPlay.tsx
+++ src/components/PlayByPlay.tsx
@@ -12,14 +12,14 @@ export function PlayByPlay({ match }: { match: Match }) {
         <Grid maxHeight={'40vh'} gridTemplateColumns={'1fr 1em 1fr'} overflowY={'scroll'}>
             {events.map((event, i) => (
                 <>
-                    {event.country === match.home_team_country && <><Text key={`time_${event.id}`} textAlign={'right'}>{event.time}</Text><Dot key={`dot_${event.id}`} finalDot={i === 0}/></>}
+                    {event.country !== match.home_team_country && <><Text key={`time_${event.id}`} textAlign={'right'}>{event.time}</Text><Dot key={`dot_${event.id}`} finalDot={i === 0}/></>}
                     <Box key={event.id} textAlign={event.country === match.home_team_country ? 'left' : 'right'}>
                         <Badge colorScheme={eventToColour(event.type_of_event)}>
                             {mapEventType(event.type_of_event)}
                         </Badge>
                         <Text>{event.player}</Text>
                     </Box>
-                    {event.country !== match.home_team_country && <><Dot key={`dot_${event.id}`} finalDot={ i === 0 }/><Text key={`time_${event.id}`}>{event.time}</Text></>}
+                    {event.country === match.home_team_country && <><Dot key={`dot_${event.id}`} finalDot={ i === 0 }/><Text key={`time_${event.id}`}>{event.time}</Text></>}
                 </>
             ))}
         </Grid>
