43502541c8d3006f517a1adeb3720902d4407a39
Pedro Santos de Mendonca <peumendonca@gmail.com>
2022-11-27 20:23:52 +0000

    Match page (#22)
    * Fix match page
    
    * Fix margins

--- src/pages/match/[id].tsx
+++ src/pages/match/[id].tsx
@@ -1,6 +1,6 @@
-import { Box, Button, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react"
-import { PlayByPlay } from "components/PlayByPlay"
-import { countryToFlag } from "flags"
+import { Box, Button } from "@chakra-ui/react"
+import Footer from "components/Footer"
+import { MatchCard } from "components/MatchCard"
 import { useTodaysMatches } from "hooks"
 import type { NextPage } from "next"
 import Link from "next/link"
@@ -19,112 +19,12 @@ const MatchStats: NextPage = () => {
   }
 
   return (
-    <Box maxW={"5xl"} mt={8} mx="auto">
-      <PlayByPlay match={match}></PlayByPlay>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th textAlign="center" fontSize="xl">
-              {countryToFlag(match?.home_team_country)}{" "}
-              {match?.home_team_country}
-            </Th>
-            <Th textAlign="center">Team Stats</Th>
-            <Th textAlign="center" fontSize="xl">
-              {countryToFlag(match?.away_team_country)}{" "}
-              {match?.away_team_country}
-            </Th>
-          </Tr>
-        </Thead>
-
-
-        <Tbody>
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.attempts_on_goal ?? 0}
-            </Td>
-            <Td textAlign="center">Shots</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.attempts_on_goal ?? 0}
-            </Td>
-          </Tr>
-
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.on_target ?? 0}
-            </Td>
-            <Td textAlign="center">Shots on target</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.on_target ?? 0}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.ball_possession ?? 0}
-            </Td>
-            <Td textAlign="center">Possession</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.ball_possession ?? 0}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.pass_accuracy ?? 0}
-            </Td>
-            <Td textAlign="center">Pass Accuracy</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.pass_accuracy ?? 0}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.fouls_committed ?? 0}
-            </Td>
-            <Td textAlign="center">Fouls</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.fouls_committed ?? 0}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.yellow_cards ?? 0}
-            </Td>
-            <Td textAlign="center">Yellow cards</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.yellow_cards ?? 0}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.red_cards ?? 0}
-            </Td>
-            <Td textAlign="center">Red cards</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.red_cards ?? 0}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.offsides ?? 0}
-            </Td>
-            <Td textAlign="center">Offsides</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.offsides ?? 0}
-            </Td>
-          </Tr>
-          <Tr>
-            <Td textAlign="center">
-              {match.home_team_statistics.corners ?? 0}
-            </Td>
-            <Td textAlign="center">Corners</Td>
-            <Td textAlign="center">
-              {match.away_team_statistics.corners ?? 0}
-            </Td>
-          </Tr>
-        </Tbody>
-      </Table>
+    <Box w={'100%'} maxW={"800px"} p={'1em'} mx="auto">
+      <MatchCard match={match} />
       <Link href={"/"}>
-        <Button mt={12}>&#8592; Dashboard</Button>
+        <Button mt={12}>&#8592; Go back</Button>
       </Link>
+      <Footer />
     </Box>
   )
 }
