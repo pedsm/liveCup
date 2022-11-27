1489ab0ae05bba97063ebcac8dade4097c9f4246
Lawand Othman <lwnd@pm.me>
2022-11-27 19:03:40 +0000

    SEO (#20)
    * install next-seo
    
    * og image
    
    * next seo with a simple default seo config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import type { AppProps } from "next/app"
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
-import '../global.css'
 import theme from "theme"
+import { DefaultSeo } from "next-seo"
+import SEO from "../next-seo.config"
+import "../global.css"
 
 const client = new QueryClient({
   defaultOptions: {
@@ -16,6 +18,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={client}>
       <ChakraProvider theme={theme}>
+        <DefaultSeo {...SEO} />
         <Component {...pageProps} />
       </ChakraProvider>
     </QueryClientProvider>
